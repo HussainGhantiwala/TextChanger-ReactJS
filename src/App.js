469d1e34efa7cda63736b7ebf744d5a07d610174
HussainGhantiwala <hussainghantiwala8@gmail.com>
2024-09-26 01:19:56 +0530

    added new functions like 'reverse text, text to speech, back to normal from reverse' and added a new component where we toggle dark mode and light mod

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 import './App.css';
+// import AboutTwo from './components/AboutTwo';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
-<>
-<Navbar title="TextChanger" homeText="Home" aboutText="About us"/> {/*This is made using props*/}
-<div className="container my-3">
-  <TextForm heading={"Enter your Text here!!"}/>
-  </div>   
-</>
+    <>
+      <Navbar title="TextChanger" homeText="Home" aboutText="About us" /> {/*This is made using props*/}
+      <div className="container my-3">
+        <TextForm heading={'Enter your text here'}/>
+        {/* <AboutTwo title={"About Us"}/> */}
+      </div>
+    </>
   );
 }
 
 export default App;
- 
